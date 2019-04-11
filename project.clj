95c4384c40f9864edc842d6662b5724c731cbd28
Caleb Black <calebmacdonaldblack@gmail.com>
2019-04-11 17:10:05 +1000

    Update with version that fails

--- project.clj
+++ project.clj
@@ -2,8 +2,7 @@
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/clojurescript "1.10.335"]
                  [reagent "0.8.1"]
-                 [thheller/shadow-cljs "2.4.24"]
-                 ;[thheller/shadow-cljs "2.8.29"]
+                 [thheller/shadow-cljs "2.8.29"]
                  [re-frame "0.10.6"]]
 
   :min-lein-version "2.5.3"
